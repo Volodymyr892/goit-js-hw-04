52d519bb7a48292f611e336b6a26d88c48bf27a5
Volodymyr892 <114510313+Volodymyr892@users.noreply.github.com>
2024-03-15 17:08:05 +0200

    о

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        
+    },
+    updatePlayTime(hours) {
+        
+    }
+    getInfo() {
+        return `${username} has ${playTime} active hours!`;
+    }
+};
